2f9e3c2f4a6e13935fde50609c4dc2123c1db3d5
Drakster-1807 <147752851+Drakster-1807@users.noreply.github.com>
2025-05-15 11:12:57 +0200

    Update Send-RestartToServer.ps1

--- Send-RestartToServer.ps1
+++ Send-RestartToServer.ps1
@@ -1,17 +1,16 @@
+# Hent gemt legitimationsoplysninger
+$cred = Get-StoredCredential -Target '192.168.1.116'
+
 # Indstillinger for serveren
 $server = "192.168.1.116"
-$username = "Administrator"  # Brug den korrekte brugernavn for serveren
-$password = "Kode1234!"   # Brug den korrekte adgangskode for serveren
-
-# Opret en PSCredential objekt
-$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username, $securePassword)
 
 # Kommando til at genstarte serveren
 $restartCommand = "shutdown.exe /r /f /t 0"  # Genstart med det samme uden ventetid
 
 # Kør kommandoen på serveren via PowerShell Remoting
-Invoke-Command -ComputerName $server -Credential $credential -ScriptBlock {
+$response = Invoke-Command -ComputerName $server -Credential $cred -ScriptBlock {
     param($cmd)
     Invoke-Expression $cmd
 } -ArgumentList $restartCommand
+
+Write-Host $response;
