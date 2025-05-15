a2beed6df98f8e1ceb75a0feb1b4c0514dc02195
sly1012 <simon.1012@hotmail.com>
2025-05-15 12:37:30 +0200

    Update Update script.ps1

--- Update script.ps1
+++ Update script.ps1
@@ -3,15 +3,15 @@ Import-Module CredentialManager
 Import-Module PSWindowsUpdate
 
 # Retrieve stored credentials
-$cred = Get-StoredCredential -Target '192.168.1.168'
+$cred = Get-StoredCredential -Target '192.168.1.116'
 
 # Check if credentials are retrieved
 if ($cred) {
     # Execute the Windows Update job on the remote server
-    Invoke-WUJob -ComputerName '192.168.1.168' -Credential $cred -Script {
+    Invoke-WUJob -ComputerName '192.168.1.116' -Credential $cred -Script {
         Import-Module PSWindowsUpdate
         Install-WindowsUpdate -AcceptAll -AutoReboot
     } -Confirm:$false -RunNow
 } else {
-    Write-Host "Credentials for 192.168.1.168 not found in Credential Manager." -ForegroundColor Red
+    Write-Host "Credentials for 192.168.1.116 not found in Credential Manager." -ForegroundColor Red
 }
