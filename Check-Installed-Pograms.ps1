0e1b33605ee2f2e8ad8c8db5cb101bb0042dfb27
sly1012 <simon.1012@hotmail.com>
2025-05-16 10:38:42 +0200

    Update Check-Installed-Pograms.ps1

--- Check-Installed-Pograms.ps1
+++ Check-Installed-Pograms.ps1
@@ -1,6 +1,19 @@
 $cred = Get-StoredCredential -Target '192.168.1.116'
+
 Invoke-Command -ComputerName '192.168.1.116' -Credential $cred -ScriptBlock {
-    Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
-    Select-Object DisplayName, DisplayVersion, Publisher | 
-    Where-Object { $_.DisplayName }
+    $results = @()
+
+    $paths = @(
+        'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
+        'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    )
+
+    foreach ($path in $paths) {
+        $items = Get-ItemProperty $path -ErrorAction SilentlyContinue |
+                 Where-Object { $_.DisplayName } |
+                 Select-Object DisplayName, DisplayVersion, Publisher
+        $results += $items
+    }
+
+    return $results
 }
