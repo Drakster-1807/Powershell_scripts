e86bdd00d50f9a00a8034489ac1326caf396faaa
sly1012 <simon.1012@hotmail.com>
2025-05-16 09:10:22 +0200

    Update Display-Message.ps1

--- Display-Message.ps1
+++ Display-Message.ps1
@@ -4,7 +4,12 @@ $cred = Get-StoredCredential -Target '192.168.1.116'
 # Define the server IP address
 $server = '192.168.1.116'
 
+# Define the message to be displayed
+$message = "This is a message from Client1."
+
 # Execute the command on the remote server
 Invoke-Command -ComputerName $server -Credential $cred -ScriptBlock {
-    Write-Host "Hello from Client1!"
-}
+    param ($msg)
+    # Send message to all users on the remote server
+    msg * /time:60 $msg
+} -ArgumentList $message
